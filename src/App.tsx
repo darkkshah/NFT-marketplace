aa2a3d23e85491fa322ff92c01893af812a06506
darkkshah <128061778+darkkshah@users.noreply.github.com>
2023-10-07 21:20:36 +0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className='hed'>
+    <>
+      <h1 className='hed1' >
+        Welcome to the Future OF Digital ART
+      </h1>
+      <p>NFTs can be traded and exchanged for money, cryptocurrencies, or other NFTs—it all depends on the value the market and owners have placed on them. For instance, you could use an exchange to create a token for an image of a banana. Some people might pay millions for the NFT, while others might think it worthless.
+      </p>
+      <div>
+        <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg" alt="" width={500} />
+          <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*iXFSD9fZ-AD73K3P.jpg" alt="" width={500} />
+          <img src="https://thedrum-media.imgix.net/thedrum-prod/s3/news/tmp/77017/untitled_design_26_11.png?w=608&ar=default&fit=crop&crop=faces,edges&auto=format&dpr=1" alt="" width={500} />
+          <img src="https://guapacho.com/wp-content/uploads/2022/08/boredape_nft-1024x577.jpg" alt="" width={500} />
+          <img src="https://assets-global.website-files.com/5e73a1e3ba24f2cd5dd2232a/64a5cc5add3e017838242781_62f66985e6fa143898ba6762_Como%2520criar%2520um%2520NFT%2520(1)%2520(1).jpeg" alt="" width={500} />
+        </div>
+        
+      </>
+    </div>
+  );
+}
+
+export default App;
